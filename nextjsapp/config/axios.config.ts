b381030d346b86a2cf35bcd8fa668825f74ddf30
Siddharth Reddy <117430424+Siddharth99990@users.noreply.github.com>
2025-09-30 12:28:31 +0530

    Update axios.config.ts

--- nextjsapp/config/axios.config.ts
+++ nextjsapp/config/axios.config.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api=axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8080/api',
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     withCredentials:true,
     timeout:10000,
     headers:{
