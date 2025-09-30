980c0386645e0fefefac4964d0c4ec5761f887d6
Siddharth99990 <117430424+Siddharth99990@users.noreply.github.com>
2025-09-30 14:14:04 +0530

    some changes

--- nextjsapp/next.config.ts
+++ nextjsapp/next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://room-management-backend-latest.onrender.com/api/:path*' // Proxy to backend
+      }
+    ];
+  }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
